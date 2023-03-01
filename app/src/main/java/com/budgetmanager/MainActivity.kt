27738b856dcb3448a7611f37d9c20c6f8b3f870c
arcanegolem <miv738@gmail.com>
2023-03-01 18:16:20 +0300

    PieChartCenterCut.kt - PieChart implementation created

--- app/src/main/java/com/budgetmanager/MainActivity.kt
+++ app/src/main/java/com/budgetmanager/MainActivity.kt
@@ -3,13 +3,12 @@ package com.budgetmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.budgetmanager.charts.PieChartCenterCut
 import com.budgetmanager.ui.theme.BudgetManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +16,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BudgetManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    PieChartCenterCut(
+                        data = mapOf(
+                            Pair("Sample-data-1", 21f),
+                            Pair("Sample-data-2", 30f),
+                            Pair("Sample-data-3", 10f),
+                            Pair("Sample-data-4", 48f),
+                            Pair("Sample-data-5", 100f),
+                            Pair("Sample-data-6", 58f)
+                        ),
+                        colors = listOf(Color.Red, Color.Blue, Color.Green, Color.Magenta, Color.Yellow, Color.Cyan)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BudgetManagerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/budgetmanager/charts/PieChartCenterCut.kt
@@ -0,0 +1,148 @@
+package com.budgetmanager.charts
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PieChartCenterCut(
+    data: Map<String, Float>,
+    outerRadius: Dp = 180.dp,
+    chartBarWidth: Dp = 40.dp,
+    chartAnimationDuration: Int = 1000,
+    colors: List<Color>,
+){
+    println(data)
+
+    val dataTotal = data.values.sum()
+    val arcValues = mutableListOf<Float>()
+
+    data.values.forEachIndexed { index, value -> arcValues.add(index, element = 360 * value / dataTotal) }
+
+    var animationExecuted by remember { mutableStateOf( false ) }
+
+    var trailingValue = 0f
+
+    val animatedSize by animateFloatAsState(
+        targetValue = if (animationExecuted) outerRadius.value else 0f,
+        animationSpec = tween(
+            durationMillis = chartAnimationDuration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        )
+    )
+
+    val animatedRotation by animateFloatAsState(
+        targetValue = if (animationExecuted) 90f * 11 else 0f,
+        animationSpec = tween(
+            durationMillis = chartAnimationDuration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        )
+    )
+
+    LaunchedEffect(true){ animationExecuted = true }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier.size(animatedSize.dp),
+            contentAlignment = Alignment.Center,
+        ){
+            Canvas(
+                modifier = Modifier
+                    .size(outerRadius * 2f)
+                    .rotate(animatedRotation),
+            ){
+                arcValues.forEachIndexed { index, value ->
+                    drawArc(
+                        color = colors[index],
+                        trailingValue,
+                        value,
+                        useCenter = false,
+                        style = Stroke(chartBarWidth.toPx(), cap = StrokeCap.Butt)
+                    )
+                    trailingValue += value
+                }
+            }
+        }
+
+        ChartLegend(data = data, colors = colors)
+    }
+}
+
+
+@Composable
+fun ChartLegend(
+    data: Map<String, Float>,
+    colors: List<Color>,
+){
+    Column(
+        modifier = Modifier
+            .padding(top = 80.dp, start = 10.dp, end = 10.dp, bottom = 10.dp)
+            .fillMaxWidth(),
+    ) {
+        data.values.forEachIndexed { index, value ->
+            ChartLegendItem(
+                itemData = Pair(data.keys.elementAt(index), value),
+                color = colors[index]
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ChartLegendItem(
+    itemData: Pair<String, Float>,
+    height: Dp = 30.dp,
+    color: Color,
+){
+
+    Surface(
+        modifier = Modifier
+            .padding(5.dp),
+        color = Color.Transparent
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = color,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .size(height)
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+            ) {
+                Text(text = itemData.first)
+                Text(text = itemData.second.toString())
+            }
+        }
+    }
+
+}
