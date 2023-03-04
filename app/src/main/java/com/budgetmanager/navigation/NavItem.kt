f0958a746be801b74fa45d095069c390b25250b6
arcanegolem <miv738@gmail.com>
2023-03-04 17:41:39 +0300

    Added navigation and Screen.kt with screens sealed class

---
+++ app/src/main/java/com/budgetmanager/navigation/NavItem.kt
@@ -0,0 +1,9 @@
+package com.budgetmanager.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem (
+    val title: String,
+    val route: String,
+    val icon: ImageVector
+)
